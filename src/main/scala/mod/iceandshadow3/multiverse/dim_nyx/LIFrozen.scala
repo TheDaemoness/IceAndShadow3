18f0e282119d1edd5ff0ae68403823eaf831763d
TheDaemoness <thedaemoness@users.noreply.github.com>
2019-09-01 11:20:37 -0700

    Item freezing now operates based on tags. Freezes: The item freezes Hot: The item does not freeze Freezes+Hot: The item gets destroyed

--- src/main/scala/mod/iceandshadow3/multiverse/dim_nyx/LIFrozen.scala
+++ src/main/scala/mod/iceandshadow3/multiverse/dim_nyx/LIFrozen.scala
@@ -9,64 +9,38 @@ import mod.iceandshadow3.lib.util.E3vl
 import mod.iceandshadow3.multiverse.DomainAlien
 
 object LIFrozen {
-	private val MAGIC_ID_BREAKS = "~:B"
-	private val MAGIC_ID_FREEZES = "~:F"
-	private val MAGIC_ID_RESISTS = "~:R"
 	private val unusualFreezeMap = new ResourceMap[String]
 
-	itemDoesNotFreeze("minecraft:blaze_rod")
-	itemDoesNotFreeze("minecraft:blaze_powder")
-	itemDoesNotFreeze("minecraft:brewing_stand")
-	itemFreezesAndChanges("minecraft:lava_bucket", "minecraft:obsidian")
-	itemFreezesAndChanges("minecraft:water_bucket", "minecraft:ice")
-	itemFreezesAndChanges("minecraft:cod_bucket", "minecraft:ice")
-	itemFreezesAndChanges("minecraft:salmon_bucket", "minecraft:ice")
-	itemFreezesAndChanges("minecraft:pufferfish_bucket", "minecraft:ice")
-	itemFreezesAndChanges("minecraft:tropical_fish_bucket", "minecraft:ice")
-	itemFreezesAndBreaks("minecraft:fire_charge")
-	itemFreezesAndBreaks("minecraft:magma_cream")
-	itemFreezesAndBreaks("minecraft:magma_block")
-	itemFreezes("minecraft:slime_ball")
-	itemFreezes("minecraft:slime_block")
-	itemFreezes("minecraft:flint_and_steel")
-	itemFreezes("minecraft:clay")
-	itemFreezes("minecraft:clay_ball")
-	itemFreezes("minecraft:campfire")
-	itemFreezes("minecraft:torch")
-	itemFreezes("minecraft:firework_rocket")
-	itemFreezes("minecraft:firework_star")
-	itemFreezes("minecraft:gunpowder")
+	val tagHot = "iceandshadow3:hot"
+	val tagFreezes = "iceandshadow3:freezes"
+
+	//TODO: Allow specifying these in recipes.
+	itemFreezesDifferently("minecraft:lava_bucket", "minecraft:obsidian")
+	itemFreezesDifferently("minecraft:water_bucket", "minecraft:ice")
+	itemFreezesDifferently("minecraft:cod_bucket", "minecraft:ice")
+	itemFreezesDifferently("minecraft:salmon_bucket", "minecraft:ice")
+	itemFreezesDifferently("minecraft:pufferfish_bucket", "minecraft:ice")
+	itemFreezesDifferently("minecraft:tropical_fish_bucket", "minecraft:ice")
 	//TODO: Also freeze items if they have a banned item in a crafting recipe (and disable this behavior for sticks).
 
-	def itemDoesNotFreeze(id: String): Unit = {
-		unusualFreezeMap += Tuple2(id, MAGIC_ID_RESISTS)
-	}
-	def itemFreezes(id: String): Unit = {
-		unusualFreezeMap += Tuple2(id, MAGIC_ID_FREEZES)
-	}
-	def itemFreezesAndBreaks(id: String): Unit = {
-		unusualFreezeMap += Tuple2(id, MAGIC_ID_BREAKS)
-	}
-	def itemFreezesAndChanges(id: String, to: String): Unit = {
+	def itemFreezesDifferently(id: String, to: String): Unit = {
 		//TODO: Check that to is valid.
 		unusualFreezeMap += Tuple2(id, to)
 	}
 	def freeze(input: WItemStack): WItemStack = {
 		val name = input.registryName
 		if(name == null) return input //Also checks if the stack is empty.
-		val resultNameOption = unusualFreezeMap.get(name)
-		if(resultNameOption.isEmpty) {
-			if(input.getDomain.resistsFreezing) input
-			else if(input.getBurnTicks > 0 || input.isAny(ItemQueries.food, ItemQueries.drink, ItemQueries.compostable)) {
-				LogicItemChameleon.createFrom(input, DomainAlien.frozen)
-			} else input
-		} else {
-			val newname = resultNameOption.get
-			if(newname.eq(MAGIC_ID_RESISTS)) input
-			else if(newname.eq(MAGIC_ID_BREAKS)) new WItemStack(null, null)
-			else if(newname.eq(MAGIC_ID_FREEZES)) LogicItemChameleon.createFrom(input, DomainAlien.frozen)
-			else WItemStack.make(newname) //Make recursive? Then we'd need to check for circular references.
-		}
+		val hottag = input.hasTag(tagHot)
+		val freezetag = input.hasTag(tagFreezes)
+		val isfuel = input.getBurnTicks > 0
+		val natfreeze = input.isAny(ItemQueries.food, ItemQueries.drink, ItemQueries.compostable) || isfuel
+		if(!input.getDomain.resistsFreezing && (freezetag || natfreeze && !hottag)) {
+			val newname = unusualFreezeMap.get(name).orNull
+			if(newname == null) {
+				if(hottag) new WItemStack(null, null)
+				else LogicItemChameleon.createFrom(input, DomainAlien.frozen)
+			} else WItemStack.make(newname)
+		} else input
 	}
 
 	def freeze(input: WItemStack, player: Option[WEntityPlayer]): Option[WItemStack] = {
