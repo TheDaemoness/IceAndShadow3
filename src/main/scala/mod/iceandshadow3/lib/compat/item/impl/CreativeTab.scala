76b6460db03b51c683e7b8a69d2307726a58c7c4
TheDaemoness <thedaemoness@users.noreply.github.com>
2019-12-29 19:14:02 -0800

    Creative tab improvements. The entries in the creative tab are sorted by unlocalized name. Enabled search on the creative tab. Shortened the English localized name to "IaS3".

--- src/main/scala/mod/iceandshadow3/lib/compat/item/impl/CreativeTab.scala
+++ src/main/scala/mod/iceandshadow3/lib/compat/item/impl/CreativeTab.scala
@@ -3,8 +3,16 @@ package mod.iceandshadow3.lib.compat.item.impl
 import mod.iceandshadow3.IaS3
 import mod.iceandshadow3.multiverse.DomainNyx
 import net.minecraft.item.{ItemGroup, ItemStack}
+import net.minecraft.util.NonNullList
 
 protected[compat] object CreativeTab extends ItemGroup(IaS3.MODID) {
+	override def fill(list: NonNullList[ItemStack]): Unit = {
+		super.fill(list)
+		list.sort((a, b) => a.getItem.getRegistryName.getPath.compareTo(b.getItem.getRegistryName.getPath))
+	}
+	override def hasSearchBar = true
+	override def getBackgroundImage = ItemGroup.SEARCH.getBackgroundImage
+
 	override def createIcon: ItemStack =
 		new ItemStack(BinderItem(DomainNyx.Items.wayfinder))
 }
